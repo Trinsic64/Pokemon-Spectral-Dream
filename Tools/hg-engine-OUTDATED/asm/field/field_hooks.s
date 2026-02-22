14098cb589040e5c9da39653851146e8e990c72e
Trinsic64 <andyleethomson@gmail.com>
2026-02-22 21:46:58 +1100

    Update 23

--- Tools/hg-engine-OUTDATED/asm/field/field_hooks.s
+++
@@ -1,6 +0,0 @@
-.text
-.align 2
-.thumb
-
-// currently no hooks needed!
-.word 0
--- Tools/hg-engine-OUTDATED/src/individual/GetMonEvolutionBattle.c
+++
@@ -1,3 +0,0 @@
-// done this way so that the extra overlay is created and linked properly.  PLEASE modify that file!
-
-#include "GetMonEvolutionInternal.c"
